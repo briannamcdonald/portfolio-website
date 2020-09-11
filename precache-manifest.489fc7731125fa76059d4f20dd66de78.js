e6ffeb6b717e49493b540bddf2207a65492f583a
Brianna McDonald <brmcdonald@mun.ca>
2020-09-11 17:04:43 -0230

    Updates

---
+++ precache-manifest.489fc7731125fa76059d4f20dd66de78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f422b76327ad35864585e65fec2ddc4",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "65247a82e49723352082",
+    "url": "/portfolio-website/static/css/main.aa689918.chunk.css"
+  },
+  {
+    "revision": "3ff1d1fd1e09a98ebdb6",
+    "url": "/portfolio-website/static/js/2.ca7b3306.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/portfolio-website/static/js/2.ca7b3306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65247a82e49723352082",
+    "url": "/portfolio-website/static/js/main.22786560.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  },
+  {
+    "revision": "1522875e531ca227074c1a227b6dea80",
+    "url": "/portfolio-website/static/media/CalculatorScreenshot.1522875e.png"
+  },
+  {
+    "revision": "87f2a7c8f7b305a0e5d82823b1d2b75b",
+    "url": "/portfolio-website/static/media/PortfolioWebsiteScreenshot.87f2a7c8.png"
+  },
+  {
+    "revision": "6e533a6c5b81167161f96d30274806a8",
+    "url": "/portfolio-website/static/media/ToDoListScreenshot.6e533a6c.png"
+  },
+  {
+    "revision": "0fc4013d2d2472d8a97996f49b5da20d",
+    "url": "/portfolio-website/static/media/background.0fc4013d.png"
+  },
+  {
+    "revision": "e02065d70de5e970c3c58b4bb9bc7076",
+    "url": "/portfolio-website/static/media/background2.e02065d7.png"
+  },
+  {
+    "revision": "670b98511f1f577da697752645c5499d",
+    "url": "/portfolio-website/static/media/cssart.670b9851.png"
+  },
+  {
+    "revision": "48fcca39b1096cdd05b451f6aa42e92c",
+    "url": "/portfolio-website/static/media/picture.48fcca39.jpg"
+  }
+]);
